3bab56bac858b8c65cec1b00642b4ca1db61c011
Eppalapally Ramu <REppalapally@hersheys.com>
2026-02-19 10:41:47 +0530

    added cards component

---
+++ blocks/form/components/cards/cards.js
@@ -0,0 +1,40 @@
+import { createOptimizedPicture } from '../../../../scripts/aem.js';
+import { subscribe } from '../../rules/index.js';
+ 
+ 
+function createCard(element, enums) {
+  element.querySelectorAll('.radio-wrapper').forEach((radioWrapper, index) => {
+    if (enums[index]?.name) {
+      let label = radioWrapper.querySelector('label');
+      if (!label) {
+        label = document.createElement('label');
+        radioWrapper.appendChild(label);
+      }
+      label.textContent = enums[index]?.name;
+    }
+    radioWrapper.querySelector('input').dataset.index = index;
+    const image = createOptimizedPicture(enums[index].image || 'https://main--afb--jalagari.aem.page/lab/images/card.png', 'card-image');
+   radioWrapper.appendChild(image);
+  });
+}
+export default function decorate(element, fieldJson, container, formId) {
+    element.classList.add('card');
+    createCard(element, fieldJson.enum);
+    subscribe(element, formId, (fieldDiv, fieldModel) => {
+        fieldModel.subscribe((e) => {
+            const { payload } = e;
+            payload?.changes?.forEach((change) => {
+                if (change?.propertyName === 'enum') {
+                    createCard(element, change.currentValue);
+                }
+            });
+        });
+ 
+        element.addEventListener('change', (e) => {
+            e.stopPropagation();
+            const value = fieldModel.enum?.[parseInt(e.target.dataset.index, 10)];
+            fieldModel.value = value.name;
+        });
+    });
+    return element;
+}
--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -60,3 +60,18 @@ function maskMobileNumber(mobileNumber) {
 export {
   getFullName, days, submitFormArrayToString, maskMobileNumber,
 };
+
+/**
+* Masks the first 5 digits of the mobile number with *
+* @param {*} mobileNumber
+* @returns {string} returns the mobile number with first 5 digits masked
+*/
+function maskMobileNumber(mobileNumber) {
+if (!mobileNumber) {
+return '';
+}
+const value = mobileNumber.toString();
+// Mask first 5 digits and keep the rest
+return ` ${'*'.repeat(5)}${value.substring(5)}`;
+}
+
